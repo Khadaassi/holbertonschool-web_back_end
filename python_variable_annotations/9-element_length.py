5039ca24256e7f3b8f229ad001ec61d416be56fe
Khadaassi <aassikhadija@yahoo.fr>
2024-04-22 23:35:56 +0200

    Refactor variable annotation in element_length function

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -6,9 +6,10 @@ argument and returns a list of tuples where each tuple’s first element is
 the string from L and the second element is the length of the string.
 """
 
-from typing import List, Tuple
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(L: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence])\
+        -> List[Tuple[Sequence, int]]:
     """Return a list of tuples where each tuple’s first element is string"""
-    return [(i, len(i)) for i in L]
+    return [(i, len(i)) for i in lst]
