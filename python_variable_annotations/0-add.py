6d2deca24ce57b2a4b20d1ce71a6c6f8f907957f
Khadaassi <aassikhadija@yahoo.fr>
2024-04-22 12:49:53 +0200

    Add documentation for add function in python_variable_annotations/0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -7,4 +7,5 @@ a float a and a float b as arguments and returns their sum as a float.
 
 
 def add(a: float, b: float) -> float:
+    """Add two floats and return the result as a float."""
     return a + b
