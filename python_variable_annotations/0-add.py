8abb3c5aabb62c9da21c1f070f8d7713195b8a01
Khadaassi <aassikhadija@yahoo.fr>
2024-04-22 12:52:26 +0200

    Fix variable annotation in add function

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 """
-type-annotated function add that takes
-a float a and a float b as arguments and returns their sum as a float.
+basic annotation.
 """
 
 
 def add(a: float, b: float) -> float:
     """Add two floats and return the result as a float."""
-    return a + b
+    return (a + b)
