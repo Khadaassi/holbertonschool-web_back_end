1ee084e034b30d2c89f10de269c551815307cb3d
Khadaassi <aassikhadija@yahoo.fr>
2024-04-25 12:09:16 +0200

    Refactor return statement in wait_random function to return a tuple

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-"""
-asynchronous coroutine that takes in an integer argument
-"""
+"""asynchronous coroutine that takes in an integer argument"""
 
 import asyncio
 import random
@@ -12,4 +10,4 @@ async def wait_random(max_delay = 0)->float:
     """return a random delay between 0 and max_delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    return delay
+    return (delay)
