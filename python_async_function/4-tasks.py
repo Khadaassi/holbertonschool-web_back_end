7749e4ec024e697e165759408db3b7c42b5317ae
Khadaassi <aassikhadija@yahoo.fr>
2024-04-25 13:19:26 +0200

    Refactor task_wait_n function to use asyncio.as_completed for better performance

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+""""""
+
+import asyncio
+import random
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    spawn wait_random n times with max_delay
+    """
+    delays = [task_wait_random(max_delay) for i in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
