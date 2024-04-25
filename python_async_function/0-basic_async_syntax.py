74d38e46005b133f7abb7dcb4603280989654f72
Khadaassi <aassikhadija@yahoo.fr>
2024-04-25 11:25:08 +0200

    Add basic async syntax example

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+"""
+This is a basic async syntax example.
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
