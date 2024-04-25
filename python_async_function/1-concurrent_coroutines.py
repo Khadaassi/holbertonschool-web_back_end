fd7e410e62c36748c6d85c831cd4c35caf3bc248
Khadaassi <aassikhadija@yahoo.fr>
2024-04-25 12:08:21 +0200

    add coroutine wait_n

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+"""
+"""
+
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    spawn wait_random n times with max_delay
+    """
+    delays = [wait_random(max_delay) for i in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
