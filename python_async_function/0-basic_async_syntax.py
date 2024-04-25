38927ee390d5071791d2476d6f88f7db7648f369
Khadaassi <aassikhadija@yahoo.fr>
2024-04-25 11:29:24 +0200

    Add return type annotation to wait_random function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 """
-This is a basic async syntax example.
+asynchronous coroutine that takes in an integer argument
 """
 
 import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay=10)->float:
+    """return a random delay between 0 and `max_delay`"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
