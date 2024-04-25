0dbc44e0edc19629060b023956601c42e7a56796
Khadaassi <aassikhadija@yahoo.fr>
2024-04-25 12:05:23 +0200

    Fix missing whitespace aroud operator

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10)->float:
+async def wait_random(max_delay = 0)->float:
     """return a random delay between 0 and max_delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
