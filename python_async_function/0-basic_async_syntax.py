1d6b7fdadd26eb59914f56b16d8087903f1fc15d
Khadaassi <aassikhadija@yahoo.fr>
2024-04-25 11:30:38 +0200

    Fix typo in return type annotation of wait_random function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,7 @@ import random
 
 
 async def wait_random(max_delay=10)->float:
-    """return a random delay between 0 and `max_delay`"""
+    """return a random delay between 0 and max_delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
