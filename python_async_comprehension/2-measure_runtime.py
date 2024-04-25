9fff97dab54cf63954b891c83c668909f378da2b
Khadaassi <aassikhadija@yahoo.fr>
2024-04-25 13:42:49 +0200

    Refactor measure_runtime function to use asyncio.run for simplicity

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+"""Measure the runtime of async_comprehension"""
+
+from typing import List
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the runtime of async_comprehension"""
+    import time
+    start = time.time()
+    await async_comprehension()
+    end = time.time()
+    return end - start
