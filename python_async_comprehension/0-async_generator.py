0f036b77ef9e168072cc430a070338f080ed3b53
Khadaassi <aassikhadija@yahoo.fr>
2024-04-25 13:51:09 +0200

    Refactor async_generator function to use Generator[float, None, None] in 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,12 +7,13 @@ then yield a random number between 0 and 10
 import asyncio
 import random
 from typing import Generator
-from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Coroutine that loops 10 times, each time asynchronously wait 1 second,
-    then yield a random number between 0 and 10"""
+async def async_generator() -> Generator[float, None, None]:
+    """ coroutine will loop 10 times
+    async_generator that takes no arguments
+    yield a random number between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
