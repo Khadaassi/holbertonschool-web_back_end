a6c7f94ce061954b697feb8efc505c0503e68103
Khadaassi <aassikhadija@yahoo.fr>
2024-05-07 21:25:24 +0200

    chore: Refactor createReportObject to use object spread syntax and arrow function

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,14 +1,6 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  for (const department of employeesList.departments) {
-    allEmployees[department] = employeesList[department].employees;
-  }
-
-  const getNumberOfDepartments = () => employeesList.departments.length;
-
   return {
-    allEmployees,
-    getNumberOfDepartments,
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments: () => Object.keys(employeesList).length,
   };
 }
