daa731cf670de6c5f957abc227b354e17596546c
Khadaassi <aassikhadija@yahoo.fr>
2024-05-10 08:12:28 +0200

    Refactor log_infos function to improve readability and consistency

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,31 +6,25 @@ from pymongo import MongoClient
 
 
 def log_infos() -> None:
-    """ Provides some stats about Nginx logs stored in MongoDB. """
+    """Provides some stats about Nginx logs stored in MongoDB."""
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient("mongodb://127.0.0.1:27017")
     nginx = client.logs.nginx
 
-    print(
-        "{} logs".format(
-            nginx.count_documents({})
-        )
-    )
+    print("{} logs".format(nginx.count_documents({})))
 
     print("Methods:")
 
     for method in methods:
         print(
-            "\tmethod {}: {}".format(
-                method, nginx.count_documents({'method': method})
-            )
+            "\tmethod {}: {}".format(method, nginx.count_documents({"method": method}))
         )
 
     print(
         "{} status check".format(
-            nginx.count_documents({'method': 'GET', 'path': '/status'})
+            nginx.count_documents({"method": "GET", "path": "/status"})
         )
     )
 
