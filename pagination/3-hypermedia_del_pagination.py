069839d10da7afaaa161ce898c81b3032d322e95
Khadaassi <aassikhadija@yahoo.fr>
2024-05-31 20:49:07 +0200

    chore: Refactor hypermedia_del_pagination.py for improved readability

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -33,7 +33,8 @@ class Server:
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+            self.__indexed_dataset = {i: dataset[i]
+                                      for i in range(len(dataset))}
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
