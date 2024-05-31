b851296aaf6a70778e5748c283b7b5cf590e970d
Khadaassi <aassikhadija@yahoo.fr>
2024-05-31 20:24:47 +0200

    chore: Update log_stats.py to use MongoDB collection as a parameter

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,37 +1,33 @@
 #!/usr/bin/env python3
-"""
-Provides some stats about Nginx logs stored in MongoDB
-"""
+""" Log stats """
+
+
 from pymongo import MongoClient
 
 
-def log_stats():
-    """
-    provides some stats about Nginx logs stored in MongoDB
-    """
+def log_stats(mongo_collection):
+    """Provides some stats about Nginx logs stored in MongoDB"""
 
-    client = MongoClient("mongodb://127.0.0.1:27017")
+    print(f"{mongo_collection.count_documents({})} logs")
 
-    db = client.logs
-    collection = db.nginx
-
-    total_logs = collection.count_documents({})
-
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {
-        method: collection.count_documents({"method": method}) for method in methods
-    }
 
-    status_check_count = collection.count_documents(
+    for method in methods:
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    stats_get_status = mongo_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method in methods:
-        print(f"\tmethod {method}:", method_counts[method])
-    print(f"{status_check_count} status check")
+    print(f"{stats_get_status} status check")
 
 
 if __name__ == "__main__":
-    log_stats()
+
+    client = MongoClient("mongodb://localhost:27017")
+    collection = client.logs.nginx
+
+    log_stats(collection)
+
+    client.close()
