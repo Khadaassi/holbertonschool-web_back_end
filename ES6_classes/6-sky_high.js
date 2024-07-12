fc13502c48aeac2d0e8a79d3c150635dcd31fd4b
Khadaassi <aassikhadija@yahoo.fr>
2024-07-12 18:01:52 +0200

    chore: Remove unnecessary code and update SkyHighBuilding class

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,20 +1,10 @@
+/* eslint-disable */
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
-    if (typeof floors !== 'number') {
-      throw new TypeError('floors must be a number');
-    }
-    this._sqft = sqft;
     this._floors = floors;
-  }
-
-  get sqft() {
-    return this._sqft;
   }
 
   get floors() {
