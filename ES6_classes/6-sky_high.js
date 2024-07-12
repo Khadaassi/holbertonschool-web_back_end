a99c764f61a054629d2eec4efd2aa2d9783583d5
Khadaassi <aassikhadija@yahoo.fr>
2024-07-12 18:04:11 +0200

    Refactor SkyHighBuilding class and remove unnecessary code

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,17 +1,28 @@
-/* eslint-disable */
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
+    // call constructor of super class (Building)
     super(sqft);
+
+    // Create objs
     this._floors = floors;
+  }
+
+  // Methods
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+
+  // Setters
+
+  // Getters
+  get sqft() {
+    return this._sqft;
   }
 
   get floors() {
     return this._floors;
-  }
-
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
