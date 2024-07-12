8960015f1ada174f9d84011423106763749b2338
Khadaassi <aassikhadija@yahoo.fr>
2024-07-12 17:51:50 +0200

    chore: Refactor log_stats.py to use MongoDB collection parameter

--- NoSQL/102-log_stats.py
+++ NoSQL/102-log_stats.py
@@ -1,60 +1,27 @@
 #!/usr/bin/env python3
-"""
-Python script that provides some stats about Nginx logs stored in MongoDB.
-"""
+"""Python script that provides some stats about Nginx logs
+stored in MongoDB"""
 from pymongo import MongoClient
 
 
-def log_infos() -> None:
-    """ Provides some stats about Nginx logs stored in MongoDB. """
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx = client.logs.nginx
-
-    print(
-        "{} logs".format(
-            nginx.count_documents({})
-        )
-    )
-
-    print("Methods:")
-
-    for method in methods:
-        print(
-            "\tmethod {}: {}".format(
-                method, nginx.count_documents({'method': method})
-            )
-        )
-
-    print(
-        "{} status check".format(
-            nginx.count_documents({'method': 'GET', 'path': '/status'})
-        )
-    )
-
-    print("IPs:")
-
-    ips = []
-
-
-    top_ips = nginx.aggregate([
-        { "$group": {  "_id" : "$ip", "total" : { "$sum": 1 }  } },
-        { "$sort": { "total": -1 } }
-    ])
-
-    i = 0
-    for ip in top_ips:
-        if i == 10:
-            break
-        print(
-            "\t{}: {}".format(
-                ip.get('_id'),
-                ip.get('total')
-            )
-        )
-        i += 1
-
 if __name__ == "__main__":
-    log_infos()
+    client = MongoClient("mongodb://localhost")
+    db = client["logs"]
+    c = db["nginx"]  # collection
+    print(f"{c.count_documents({})} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print(f"\tmethod {method}: {c.count_documents({'method': method})}")
+    msg = "status check"
+    print(f"{c.count_documents({'method': 'GET', 'path': '/status'})} {msg}")
+    print("IPs:")
+    sor = c.aggregate(
+        [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10},
+        ]
+    )
+    for ip in sor:
+        print(f"\t{ip['_id']}: {ip['count']}")
